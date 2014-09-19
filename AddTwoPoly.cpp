832b254cd031153ee1682564a24d7116c3a6f3b1
Glaf43 <hdnf39@mail.umkc.edu>
2014-09-19 16:06:37 -0500

    Update AddTwoPoly.cpp

--- AddTwoPoly.cpp
+++ AddTwoPoly.cpp
@@ -3,42 +3,42 @@
 #include <list>
 using namespace std;
 
-Term AddTerm( Term* Itr_1; Term* Itr_2)
+list<Term> AddPoly( const list<Term>& List_1, const list<Term>& List_2)
 {
-	if (Itr_1==NULL || Itr_2==NULL)
-		//throw error, not sure about this code
-		throw error;
-	else
+	list<Term>::const_iterator Itr1 = List_1.begin();
+	list<Term>::const_iterator Itr2 = List_2.begin();
+
+	list<Term> result;
+	Term temp;
+	while (Itr1!= List_1.end() && Itr2!=List_2.end())
 	{
-		list<Term> result;
-		Term temp;
-		while (Itr_1!=NULL && Itr_2!=NULL)
+		//comparison sign 
+		if (Itr1->get_exponent() > Itr2->get_exponent())
 		{
-			//comparison sign 
-			if ( *Itr_1>*Itr_2)
-			{
-				result.push_back(*Itr_1);
-				Itr_1++;
-			}
-			else if (*Itr_1<*Itr_2)
-			{
-				result.pish_back(*Itr_2);
-				Itr_2++;
-			}
-			else
-			{
-				temp=Term(*Itr_1.coefficient+*Itr_2.coefficient,*Itr_1.exponent);
-				result.push_back(temp);
-			}
+			result.push_back(*Itr1);
+			Itr1++;
 		}
-		while (itr2 != list_2.end()){
-			result.push_back(*itr2);
-			++itr2;
+		else if (Itr1->get_exponent() < Itr2->get_exponent())
+		{
+			result.push_back(*Itr2);
+			Itr2++;
 		}
-		while (itr1 != list_1.end()){
-			result.push_back(*itr1);
-			++itr1;
+		else
+		{
+			temp=Term(Itr1->get_coefficient()+Itr2->get_coefficient(),Itr1->get_exponent());
+			result.push_back(temp);
+			Itr1++;
+			Itr2++;
 		}
-		//not sure it should return a list or pointer to head
-		return Term;
 	}
+	while (Itr2 != List_2.end()){
+		result.push_back(*Itr2);
+		++Itr2;
+	}
+	while (Itr1 != List_1.end()){
+		result.push_back(*Itr1);
+		++Itr1;
+	}
+	//not sure it should return a list or pointer to head
+	return result;
+}
