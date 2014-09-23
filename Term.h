a14235463c53a45084ea174b20c6dd1db67e8264
Justin Karnes <jrkn87@mail.umkc.edu>
2014-09-22 20:35:18 -0500

    Update Term.h
    Updated Term.h to reflect the Term.cpp changes

--- Term.h
+++ Term.h
@@ -9,7 +9,6 @@ class Term{
 private:
 	int coefficient; // The coefficient of the term. Negative terms will have negative coefficients
 	int exponent; // The exponent of the term. This is always positive.
-	bool exponent_negative; // If an exponent is negative, this will be true
 public:
 	Term();
 	Term(int, int);
@@ -18,13 +17,12 @@ public:
 	// Get and Set functions
 	int get_coefficient(void) const; // returns the coefficient of the term.
 	int get_exponent(void) const; // returns the exponent of the term. This number can be negative
-	bool is_exponent_negative(void) const; // Evaluates to True is the term has a negative exponent
 	void set_coefficient(int); // Sets the coefficient of a Term
 	void set_exponent(int); // Sets the exponent of a term. This can accept a negative number
-	void set_negative_flag(bool); // Sets the negative exponent flag to the boolean value passed into this function
+	
 	// Operator Definitions
 	const Term operator+(const Term& L_term); // Adds two terms together
-	//////////////////////////////////////////////
+	
 	// An's update
 	bool Term::operator<(const Term& L_term);
 	bool Term::operator>(const Term& L_term); 
