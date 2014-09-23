fa8190ed11bf1b46fc9629db538b9280874b6edd
atn4z7 <atn4z7@mail.umkc.edu>
2014-09-22 21:32:45 -0500

    Update AddTwoPoly.cpp

--- AddTwoPoly.cpp
+++ AddTwoPoly.cpp
@@ -1,5 +1,4 @@
 //Hoang Nguyen
-
 #include <iostream>
 #include <vector>
 #include <list>
@@ -7,43 +6,41 @@
 #include "String_Tokenizer.h"
 using namespace std;
 
-//this function add 2 polynomials to one new polinomial
-//take 2 lists and return 1 new list
+//this function adds 2 polynomials and returns the result as a new polynomial
 list<Term> AddPoly( const list<Term>& List_1, const list<Term>& List_2)
 {
-	//iterator
+	//iterators
 	list<Term>::const_iterator Itr1 = List_1.begin();
 	list<Term>::const_iterator Itr2 = List_2.begin();
-	//call new list
+	//new list
 	list<Term> result;
-	//a temp term when 2 term have the same exponent
+	//a temp term to store the adding
 	Term temp;
-	//continue till one iterator point the tail of the list
+	//continue till one iterator points to the tail of the list
 	while (Itr1!= List_1.end() && Itr2!=List_2.end())
 	{
 		//add the term with a bigger exponent to the new list
-		if (Itr1->get_exponent() > Itr2->get_exponent())
+		if ((Term)*Itr1 > (Term)*Itr2)
 		{
 			result.push_back(*Itr1);
 			Itr1++;
 		}
-		else if (Itr1->get_exponent() < Itr2->get_exponent())
+		else if ((Term)*Itr1 < (Term)*Itr2)
 		{
 			result.push_back(*Itr2);
 			Itr2++;
 		}
-		//if 2 term have the same exponent, 
-		//create a new term with addition of coeffecients and the same exponent
-		//add it to new list
+		//if 2 term have the same exponent, just add them and add the result to new list
 		else
 		{
-			temp=Term(Itr1->get_coefficient()+Itr2->get_coefficient(),Itr1->get_exponent());
-			result.push_back(temp);
+			temp = (Term)*Itr1+(Term)*Itr2;
+			if (temp.get_coefficient() != 0) // only add to the result if the term has non-zero coefficient
+				result.push_back(temp);
 			Itr1++;
 			Itr2++;
 		}
 	}
-	//Add terms left to new list
+	//Add any remaining terms to the new list
 	while (Itr2 != List_2.end()){
 		result.push_back(*Itr2);
 		++Itr2;
