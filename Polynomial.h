da092a358b37f2502438e68f753bf3bec404271e
atn4z7 <atn4z7@mail.umkc.edu>
2014-09-25 11:28:09 -0500

    Create new file

---
+++ Polynomial.h
@@ -0,0 +1,29 @@
+/*
+An Nguyen + Hoang Nguyen
+Polynomial Class header file.
+*/
+
+#ifndef POLYNOMIAL_H
+#define POLYNOMIAL_H
+
+#include <string> 
+#include <list>
+#include "Term.h"
+#include "String_Tokenizer.h"
+
+class Polynomial{
+private:
+	list<Term> List_Terms; //a list that store terms
+public:
+	//constructor
+	Polynomial();
+	Polynomial(string& poly);
+	//destructor
+	~Polynomial();
+	//operator Definitions
+	const Polynomial operator+(const Polynomial& L_term) const; // Adds two polynomials together
+	//output function
+	friend ostream& operator<<(ostream& output,const Polynomial& poly);
+};
+
+#endif
