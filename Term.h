305f7dcbaaa1054a27914f35e5276d8064b705a9
atn4z7 <atn4z7@mail.umkc.edu>
2014-09-25 11:26:43 -0500

    Update Term.h

--- Term.h
+++ Term.h
@@ -2,36 +2,42 @@
 Justin Karnes
 Term Class header file.
 */
-#pragma once
-using namespace std;
+
+#ifndef TERM_H
+#define TERM_H
+
 #include <iostream>
+#include <stdexcept>
+using namespace std;
+
 class Term{
 private:
 	int coefficient; // The coefficient of the term. Negative terms will have negative coefficients
 	int exponent; // The exponent of the term. This is always positive.
-	//bool exponent_negative; // If an exponent is negative, this will be true
 public:
+	// Constructor
 	Term();
 	Term(int, int);
+	// Destructor
 	~Term();
 
 	// Get and Set functions
 	int get_coefficient(void) const; // returns the coefficient of the term.
 	int get_exponent(void) const; // returns the exponent of the term. This number can be negative
-	//bool is_exponent_negative(void) const; // Evaluates to True is the term has a negative exponent
 	void set_coefficient(int); // Sets the coefficient of a Term
 	void set_exponent(int); // Sets the exponent of a term. This can accept a negative number
-	//void set_negative_flag(bool); // Sets the negative exponent flag to the boolean value passed into this function
+
 	// Operator Definitions
-	const Term operator+(const Term& L_term); // Adds two terms together
-	//////////////////////////////////////////////
-	// An's update
-	//comparison operators to comppare exponents
+	const Term operator+(const Term& L_term) const; // Adds two terms together
+	// Comparison operators to comppare exponents
 	bool Term::operator<(const Term& L_term);
 	bool Term::operator>(const Term& L_term); 
 	bool Term::operator==(const Term& L_term);
-	//output function
-	friend ostream& operator<<(ostream& output, const Term& aTerm);
-	//assignment operator
+	// Assignment operator
 	const Term& Term::operator=(const Term& L_term);
+
+	// Output function
+	friend ostream& operator<<(ostream& output, const Term& aTerm);
 };
+
+#endif
